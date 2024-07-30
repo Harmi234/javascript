66dccd246c12799eb1112adaff956635876ff77f
Your Name <harmiraval88@gmail.com>
2024-07-30 16:47:46 +0530

    exam-5

--- exam-5/task-10/index.js
+++ exam-5/task-10/index.js
@@ -1,8 +1,7 @@
 //Find Missing Number
 function missingnumber(array){
     let n = array.length+1;
-    let extrasum =  n*(n+1)/2;
-    let  rezultSum = array((sum, num) => sum + num, 0); 
-    return extrasum - rezultSum;
+    let sum =  n*(n+1)/2;
+    
 }
 console.log(missingnumber([1, 2, 4, 5, 6])); 
