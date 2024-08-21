6a0b84d3b7b02a4f16c8f05d32033b94da3d7d01
Your Name <harmiraval88@gmail.com>
2024-08-21 14:58:00 +0530

    dice

---
+++ dice/index.js
@@ -0,0 +1,6 @@
+document.getElementById("rollButton").addEventListener("click", function() {
+    const dice = document.getElementById("dice");
+    const diceFaces = ["1","2","3","4","5","5"];
+    const randomIndex = Math.floor(Math.random() * diceFaces.length);
+    dice.textContent = diceFaces[randomIndex];
+});
