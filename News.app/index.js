f8a1368c507ec217585119267732539c45a40c4c
Your Name <harmiraval88@gmail.com>
2024-08-29 13:48:56 +0530

    news.app

--- News.app/index.js
+++ News.app/index.js
@@ -1,31 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let userForm = document.getElementById('userForm');
-    let userInfo = document.getElementById('userInfo');
+    let userForm = JSON.parse(localStorage.getItem("userForm")) || [];
 
-    userForm.addEventListener('submit', (e) => {
+    const handleDelete = (index) => {
+        userForm.splice(index, 1);  
+        localStorage.setItem("userForm", JSON.stringify(userForm));  
+        mapper(userForm);  
+    }
+
+    const mapper = (userForm) => {
+        document.getElementById("userinfo").innerHTML = ""; 
+
+        userForm.forEach((ele, index) => {
+            let title = document.createElement("h3");
+            title.innerHTML = ele.title;
+            let img = document.createElement("img");
+            img.src = ele.img;
+            img.style.maxWidth = "200px";
+            let category = document.createElement("p");
+            category.innerHTML = ele.category;
+            let btnDlt = document.createElement("button");
+            btnDlt.innerHTML = "Delete";
+            btnDlt.addEventListener("click", () => handleDelete(index));  
+            let div = document.createElement("div");
+            div.append(img, title, category, btnDlt);
+            document.getElementById("userinfo").append(div);
+        });
+    };
+
+    document.getElementById('userForm').addEventListener('submit', (e) => {
         e.preventDefault(); 
 
         let name = document.getElementById('name').value;
-        let email = document.getElementById('email').value;
         let imgUrl = document.getElementById('imgUrl').value;
-        let password = document.getElementById('password').value;
         let country = document.getElementById('country').value;
 
-        userInfo.innerHTML = `
-            <h3>User Information</h3>
-            <p>Name: ${name}</p>
-            <p>Email: ${email}</p>
-            <p>Image: <img src="${imgUrl}" width="100"></p>
-            <p>Password: ${'*'.repeat(password.length)}</p>
-            <p>Country: ${country}</p>
-            <button id="deleteBtn">Delete</button>
-        `
-        
-        document.getElementById('deleteBtn').addEventListener('click', () => {
-            userInfo.innerHTML = ''; 
-        });
+        let newUser = {
+            title: name,
+            img: imgUrl,
+            category: country
+        };
 
-        userForm.reset();
+        userForm.push(newUser);
+        localStorage.setItem("userForm", JSON.stringify(userForm));
+
+        mapper(userForm);
+
+        e.target.reset();
     });
-});
 
+    mapper(userForm);
+});
