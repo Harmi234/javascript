8a8e8eae3f2bda657c893ddcf25a6fbac06d7a69
Your Name <harmiraval88@gmail.com>
2024-09-23 22:57:10 +0530

    API exam-1

--- API exam-1/index.js
+++ API exam-1/index.js
@@ -1,10 +1,14 @@
+let productsData = [];  
+let filteredProducts = []; 
+
 console.log("starting");
 
 const getData = async () => {
-        let req = await fetch("https://dummyjson.com/products");
-        let res = await req.json();
-        console.log(res);
-        mapper(res.products); 
+    let req = await fetch("https://dummyjson.com/products");
+    let res = await req.json();
+    productsData = res.products; 
+    filteredProducts = productsData; 
+    mapper(filteredProducts);
 };
 
 getData();
@@ -12,6 +16,9 @@ getData();
 console.log("setting stat");
 
 const mapper = (data) => {
+    const productList = document.getElementById("productList");
+    productList.innerHTML = ''; 
+
     data.map((item) => {
         let productContainer = document.createElement("div");
         productContainer.className = "product";
@@ -20,11 +27,11 @@ const mapper = (data) => {
         title.innerHTML = item.title;
 
         let img = document.createElement("img");
-        img.src = item.image;
+        img.src = item.thumbnail;  
         img.alt = item.title;
 
         let price = document.createElement("p");
-        price.innerHTML = `Price: ${item.price}`;
+        price.innerHTML = `Price: $${item.price}`;
 
         let category = document.createElement("p");
         category.innerHTML = `Category: ${item.category}`;
@@ -32,17 +39,49 @@ const mapper = (data) => {
         let rating = document.createElement("p");
         rating.innerHTML = `Rating: ${item.rating}`;
 
-        let brand = document.createElement("brand");
-        brand.innerHTML = item.brand;
+        let brand = document.createElement("p");
+        brand.innerHTML = `Brand: ${item.brand}`;
 
-        let discountPercentage = document.createElement("discountPercentage");
-        discountPercentage.innerHTML = item.discountPercentage;
-
-
-
-        productContainer.append(title, img, price, category, rating,brand);
-        document.getElementById("productList").append(productContainer);
+        productContainer.append(title, img, price, category, rating, brand);
+        productList.append(productContainer);
     });
 };
+
+// Search
+document.getElementById("searchInput").addEventListener("input", (e) => {
+    const searchTerm = e.target.value.toLowerCase();
+    filteredProducts = productsData.filter(item => item.title.toLowerCase().includes(searchTerm));
+    mapper(filteredProducts);
+});
+
+// Sorting
+const handleSort = (orderBy) => {
+    let sortedData = [...filteredProducts];
+    if (orderBy == "price") {
+        sortedData.sort((a, b) => a.price - b.price);
+    } else if (orderBy == "rating") {
+        sortedData.sort((a, b) => b.rating - a.rating);
+    } else if (orderBy == "title") {
+        sortedData.sort((a, b) => a.title.localeCompare(b.title));
+    }
+    mapper(sortedData);
+};
+
+// Filtering
+const handleFilter = (category) => {
+    if (category) {
+        filteredProducts = productsData.filter((ele) => ele.category == category);
+    } else {
+        filteredProducts = productsData;
+    }
+    mapper(filteredProducts);
+};
+
+document.getElementById("sortSelect").addEventListener("change", (e) => {
+    handleSort(e.target.value);
+});
+document.getElementById("filterSelect").addEventListener("change", (e) => {
+    handleFilter(e.target.value);
+});
 
 console.log("ending");
